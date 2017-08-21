8598b33b00dd1480e28e69cc6b43bc643b447b27
chrisw310 <cw@fea.st>
2017-08-21 20:21:33 +1000

    Started Again

--- Bubbler/bubbler.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.bodywrap').css("opacity", "0"); 
-    $('.bodywrap').animate({ // initial
-    opacity: "1", // appear
-    }, 1000, 'easeOutCubic', function() {});
-});
